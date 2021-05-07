5691cf7a7c7d08107e8e12c78a86df9fc0d18df9
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-05-07 20:51:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 with open('main.txt') as f:
     lines = f.readlines()
-a = input("just test: ").lower()
+a = input("you want capital of which country: ").lower()
 
 def lower_dict(d):
    new_dict = dict((k.lower(), v.lower()) for k, v in d.items())
    return new_dict
-
 currencyDict = {}
 for line in lines:
     parsed = line.split("\t")
     currencyDict[parsed[0]] = parsed[1]
     currencyDict = lower_dict(currencyDict)
-print(currencyDict[a])
+try:
+    print(currencyDict[a])
+   except:
+    raise Exception(f'''couldn't find capital for {a}''')
 
